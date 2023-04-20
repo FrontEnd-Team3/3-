5daa739ff4e9eaa717871efcbd7fd10cea7b2620
yunseungbin <08jmda@gmail.com>
2023-04-20 20:42:11 +0900

    래벨 3 다품

--- level3/level3.js
+++ level3/level3.js
@@ -1,18 +1,20 @@
-/*
-goals
-2. 재료에 이미 있는 value값을 재료명 input에 입력할시 alert("이미 존재하는 재료입니다") 
-*/
-
 const $form = document.querySelector("#ingredient-form");
 const $input1 = document.getElementsByName("ingredient");
 const $input2 = document.getElementsByName("weight");
 const $table = document.querySelector("table");
+const $material = document.getElementsByClassName("material");
 
 function AddMaterial(e) {
   e.preventDefault();
   let inputvalue1 = $input1[0].value;
   let inputvalue2 = $input2[0].value;
   if (inputvalue1 !== "" && inputvalue2 !== "") {
+    for (let i = 0; i < $material.length; i++) {
+      if ($material[i].innerText === inputvalue1) {
+        alert("이미존재하는 재료입니다");
+        return ($input1[0].value = ""), ($input2[0].value = "");
+      }
+    }
     const $tr = document.createElement("tr");
     $table.appendChild($tr);
     $tr.innerHTML = `
@@ -39,11 +41,11 @@ const $ul = document.querySelector("#ingredient-list");
 function AddLiTag() {
   const $td1 = document.querySelectorAll(".td1");
   const $td2 = document.getElementsByClassName("td2");
+  $ul.innerHTML = "";
   for (let i = 0; i < $td1.length; i++) {
     const $li = document.createElement("li");
     $ul.appendChild($li);
     $li.innerText = `${$td1[i].innerText} : ${$td2[i].innerText}`;
-    // 제출했을때 $td1.length만큼 추가되는 것이 아니라 $td1.length만큼 수정되게끔 해야댐 여기 수정!!!!
   }
 }
 
